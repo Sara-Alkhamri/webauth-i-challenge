0272368d438499f6b72afd58ff1ac49455def00e
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-15 14:25:47 -0700

    Imported and implemented connect-session-knex

--- server.js
+++ server.js
@@ -3,19 +3,29 @@ const UsersRouter = require('./users/users-router')
 const server = express();
 
 const session = require('express-session') //import experess-session
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const knexConfig = require('./data/db-config')
 
 //session object
 const sessionConfig = {
-    name: 'newSesh',
-    secret: 'Keep it secret',
+    name: 'newSesh', // default would be "sid"
+    secret: 'keep it secret, keep it safe!', // use an environment variable for this
     cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60,
-        secure: false,
-      },
-      resave: false,
-      saveUninitialized: true,
-}
+      httpOnly: true, // JS cannot access the cookies
+      maxAge: 1000 * 60 * 60, // expiration time in milliseconds
+      secure: false, // use cookie over HTTPS only. Should be true in production
+    },
+    resave: false,
+    saveUninitialized: true, // read about GDPR compliance about cookies
+  
+    // change to use our database instead of memory to save the sessions
+    store: new KnexSessionStore({
+      knex: knexConfig,
+      createtable: true, // automatically create the sessions table
+      clearInterval: 1000 * 60 * 30, // delete expired sessions every 30
+    }),
+  };
 
 server.use(express.json());
 server.use(session(sessionConfig))
