5811af36c4896c230c5d7974bf316b493c04c5a5
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-15 14:20:54 -0700

    Added express-session and created sessionConfig object

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const express = require('express');
 const UsersRouter = require('./users/users-router')
 const server = express();
 
+const session = require('express-session') //import experess-session
+
+//session object
+const sessionConfig = {
+    name: 'newSesh',
+    secret: 'Keep it secret',
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+      },
+      resave: false,
+      saveUninitialized: true,
+}
+
 server.use(express.json());
+server.use(session(sessionConfig))
 
 server.use('/api', UsersRouter)
 
